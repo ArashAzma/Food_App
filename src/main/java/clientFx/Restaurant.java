bdd2580b06f69df224a62fe325cf6d3caaa92a0f
baharmohammadi <baharmohammadisani83@gmail.com>
2023-06-01 19:51:36 +0430

    trying iif i can push

--- src/main/java/clientFx/Restaurant.java
+++ src/main/java/clientFx/Restaurant.java
@@ -6,8 +6,19 @@ public abstract class Restaurant {
     private String name;
     private String address;
     private ArrayList<String> menu;
-    public Restaurant(String name, String address){
+    private String time;
+    private boolean is_takeAway;
+    private int tableCount = 0;
+    private int courierCount = 0;
+    public Restaurant(String name, String address, String time, boolean is_takeAway, int count){
         this.name = name;
         this.address = address;
+        this.time = time;
+        this.is_takeAway = is_takeAway;
+        if (is_takeAway) {
+            this.courierCount = count;
+        } else {
+            this.tableCount = count;
+        }
     }
 }
