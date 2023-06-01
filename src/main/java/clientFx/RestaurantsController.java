4dfcea95fb9e4ee7c06d280b60fb4611fbc95590
ArashAzma <arashazma3@gmail.com>
2023-06-01 16:57:15 +0330

    added image as an button

--- src/main/java/clientFx/RestaurantsController.java
+++ src/main/java/clientFx/RestaurantsController.java
@@ -1,4 +1,50 @@
 package clientFx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RestaurantsController {
+    private static Stage stage;
+    private static Scene scene;
+    private Parent root;
+    @FXML
+    private ImageView r1;
+    @FXML
+    private ImageView r2;
+    @FXML
+    private ImageView r3;
+    @FXML
+    protected void restaurant1(MouseEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("restaurantPageView.fxml"));
+        root = loader.load();
+        switchToScene(e, "restaurantPageView.fxml", root);
+    }
+    @FXML
+    protected void restaurant2(MouseEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("restaurantPageView.fxml"));
+        root = loader.load();
+        switchToScene(e, "restaurantPageView.fxml", root);
+    }
+    @FXML
+    protected void restaurant3(MouseEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("restaurantPageView.fxml"));
+        root = loader.load();
+        switchToScene(e, "restaurantPageView.fxml", root);
+    }
+    private static void switchToScene(MouseEvent event, String Scene, Parent root)  throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource(Scene));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
