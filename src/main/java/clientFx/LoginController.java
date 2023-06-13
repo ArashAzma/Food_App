2c7f3111449dd5d8d72521dc211839dded1e6a9f
ArashAzma <arashazma3@gmail.com>
2023-06-13 12:54:03 +0330

    visual upgrades

--- src/main/java/clientFx/LoginController.java
+++ src/main/java/clientFx/LoginController.java
@@ -2,14 +2,17 @@ package clientFx;
 
 import common.Restaurant;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -33,14 +36,12 @@ public class LoginController extends Main{
     @FXML
     private Label passError;
 
+
     @FXML
     private void login(ActionEvent e)throws IOException{
         try {
             System.out.println("Connected to server.");
             System.out.println("socket = " + socket);
-//            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-//            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-//            ObjectOutputStream.reset().
             outputStream.flush();
 
             String name = nameBox.getText();
